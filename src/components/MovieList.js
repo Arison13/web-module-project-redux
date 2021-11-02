e09b72907fc3c87111a312c79663379211be7817
Arison13 <arisonarias@gmail.com>
2021-11-02 18:39:28 -0400

    created store and provider in index js, connected movieList component to state thru redux

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
 const MovieList = (props)=> {
     const movies = [];
-
+    console.log("MOVIELIST:", props)
     return (
         <div className="col">
             <table className="table table-striped table-hover">
@@ -21,7 +23,7 @@ const MovieList = (props)=> {
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        props.movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
                     }
                 </tbody>
             </table>
@@ -30,5 +32,9 @@ const MovieList = (props)=> {
         </div>
     );
 }
-
-export default MovieList;
+const mapStateToProps = (state) => {
+    return ({
+        movies:state.movies
+    })
+}
+export default connect(mapStateToProps)(MovieList);
--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider} from 'react-redux';
 
-import reducer from './reducers';
+import reducer from './reducers/movieReducer';
 
 import App from './App'
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+
+const store = createStore(reducer);
+console.log(store.getState());
+
 ReactDOM.render(
-  <Router>
+  <Provider store={store}> 
+    <Router>
       <App />
-  </Router>,
+    </Router>
+  </Provider>,
   document.getElementById('root')
 );
