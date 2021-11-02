94c903a5cf981c2eecb6085d204c1a59532aa3e3
Arison13 <arisonarias@gmail.com>
2021-11-02 19:40:43 -0400

    got deleteMovie and addMovie funcionality working

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,14 +3,21 @@ import { useParams, useHistory } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 
+import { deleteMovie } from '../actions/movieActions';
+
 const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
     
-    // console.log("MOVIE PROPS:", props)
+    console.log("MOVIE PROPS:", props)
  
     const movies = props.movies;
     const movie = movies.find(movie=>movie.id===Number(id));
+
+    const handleDelete = () => {
+        props.deleteMovie(movie.id)
+        push('/movies')
+    }
     return(<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -41,7 +48,7 @@ const Movie = (props) => {
                         
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" onClick={handleDelete}/></span>
                         </section>
                     </div>
                 </div>
@@ -55,4 +62,4 @@ const mapStateToProps = (state) => {
         movies : state.movies
     })
 }
-export default connect(mapStateToProps)(Movie);
+export default connect(mapStateToProps, {deleteMovie} )(Movie);
